124cc576d883fddb85673d81870c5df49d0385ef
Scott Solmonson <ssolmonson63@gmail.com>
2021-02-21 12:37:44 -0500

    Create second block of landing page
    Created a rough draft for the second block of the landing page.
    Information and images in this file are strictly placeholder. With
    limited information on the what this will hold, class names have been
    named very basically and are not as descriptive as usual.

--- src/components/Block2/Block2.js
+++ src/components/Block2/Block2.js
@@ -2,12 +2,38 @@ import React from "react";
 
 function Block2() {
   return (
-      <div>
-          Blcok 2
+      <div className="block2-background">
+          <div className="block2-container">
+            <div className="block2-heading">
+              <h2>How we determine areas in need of retrofitting.</h2>
+              <p>Words words, blah blah blah. </p>
+            </div>
+            <div className="block2-imagesBox">
+              <div className="imageBox1">
+                <img src="https://www.fillmurray.com/228/212" width="228" height="212" alt="imageBox1" className="" />
+                <h4>Energy efficiency</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim, mauris sed facilisis
+                ultricies, neque tortor viverra diam, a lacinia tortor lacus sit amet nibh. Maecenas volutpat tellus
+                at tempor mattis. Donec quis est turpis.</p>
+              </div>
+              <div className="imageBox2">
+                <img src="https://placebeard.it/228x212" width="228" height="212" alt="imageBox2" className="" />
+                <h4>Energy efficiency</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim, mauris sed facilisis
+                ultricies, neque tortor viverra diam, a lacinia tortor lacus sit amet nibh. Maecenas volutpat tellus
+                at tempor mattis. Donec quis est turpis.</p>
+              </div>
+              <div className="imageBox3">
+                <img src="https://picsum.photos/228/212" width="228" height="212" alt="imageBox3" className="" />
+                <h4>Energy efficiency</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dignissim, mauris sed facilisis
+                ultricies, neque tortor viverra diam, a lacinia tortor lacus sit amet nibh. Maecenas volutpat tellus
+                at tempor mattis. Donec quis est turpis.</p>
+              </div>
+            </div>
+          </div>
       </div>
   );
 }
 
 export default Block2;
-
-  
