feed637656b39759832c51076780204c9cfe8632
Scott Solmonson <ssolmonson63@gmail.com>
2021-02-22 13:59:31 -0500

    Replace filler content
    Edited content on Block3 to match the text and images specified by UX
    figma design.

--- src/components/Block3/Block3.js
+++ src/components/Block3/Block3.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import './Block3.css'
+import skylineBoston from './images/skylineBoston.png'
 
 function Block3() {
   return (
@@ -8,24 +9,18 @@ function Block3() {
         <div className="block3-container">
           <div className="block3-textBox">
             <div className="block3-large-text">
-              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar
-              enim non nulla egestas, sed dignissim dui blandit. Interdum et malesuada fames
-              ac ante ipsum primis in faucibus. Donec malesuada, ligula ac semper convallis,
-              nulla ex ullamcorper nisl, id venenatis nunc felis vel eros. Integer.</h3>
+              <h3>How do we determine which areas are most in need of retrofitting?</h3>
             </div>
             <div className="block3-small-text">
               <div className="block3-box-text">
-                <p>Phasellus id libero in metus venenatis faucibus. Morbi posuere vehicula vehicula.
-                Fusce vel erat id ex eleifend consequat.</p>
-              </div>
-              <div className="block3-truck-text">
-                <p>Donec accumsan eu enim et ultricies. Phasellus viverra, massa nec posuere tincidunt,
-                ligula sapien pharetra mauris, nec condimentum ex sapien sed ligula.</p>
+                <p>The City of Boston has a Building Energy Reporting and Disclosure Ordinance
+                (BERDO) that requires buildings to disclose their annual water and energy usage.
+                This data, along with building history, helps us determine which buildings need to go green.</p>
               </div>
             </div>
           </div>
           <div className="block3-image">
-            <img src="http://placeimg.com/540/440/nature" width="540" height="440" alt="beautiful landscape" className="block3Image" />
+            <img src={skylineBoston} width="357" height="536" alt="beautiful landscape" className="block3Image" />
           </div>
         </div>
       </div>
